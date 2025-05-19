6a450dd3c584242d1dee6a2b5c3e4b4ddd4841d0
ViniciusLima94 <vinicius.lima.cordeiro@gmail.com>
2025-05-19 16:02:57 +0200

    up

--- .ipynb_checkpoints/two_nodes-checkpoint.py
+++ .ipynb_checkpoints/two_nodes-checkpoint.py
@@ -2,9 +2,34 @@ import jax
 import matplotlib.pyplot as plt
 import numpy as np
 import xarray as xr
-from frites.core import copnorm_nd
+from frites.core import copnorm_nd, gcmi_nd_cc
 from hoi.core import get_mi
 from mne.time_frequency.tfr import tfr_array_morlet
 from tqdm import tqdm
 
 from src.models import simulate, simulate_delayed
+
+########################################################################################
+#### Simulation parameters
+########################################################################################
+
+Nareas = 2
+ntrials = 200
+fsamp = 1 / 1e-4
+
+time = np.arange(-4, 4, 1 / fsamp)
+beta = 1e-4
+Npoints = len(time)
+decim = 20
+
+C = np.array([[1, 0], [0, 1]]).T
+
+# For the node receiving the input
+f = 40
+a = -5
+
+Iext = np.zeros((Nareas, Npoints))
+Iext[0, (time >= 0) & (time <= 0.4)] = 1
+Amplitudes = np.linspace(0, 0.1, ntrials)
+CS = Amplitudes[..., None, None] * Iext
+seeds = np.random.randint(0, 10000, ntrials)
